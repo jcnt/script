6a53b66b616475359d463a3187c3427e8e69e1ea
Jacint Juhasz <yyaazz@gmail.com>
2024-02-25 19:40:38 +0000

    new file:   deploy-killer.py

---
+++ deploy-killer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+#
+#
+#
+# simple python script to kill a random K8s pod in every 5 seconds
+# handy to see how deployments and replicasets working
+
+
+import os
+from random import choice
+from time import sleep
+
+to_kill = ""
+
+def find_pod(): 
+  global to_kill
+  podlist = []
+  pods = os.popen("kubectl get pods --no-headers|awk {'print $1'}").read()
+  for i in pods.splitlines(): 
+    podlist.append(i)
+  to_kill = choice(podlist)
+
+while True: 
+  find_pod()
+  print("\nKilling pod")
+  os.system(f"kubectl delete pod {to_kill}")
+  print("\nSleeping for 10sec...  hit ctrl+c to stop\n")
+  sleep(10)
+
+

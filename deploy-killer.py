0dba14c6cfe9c266448f5c10455eb93fb7ccc86d
Jacint Juhasz <yyaazz@gmail.com>
2024-02-26 20:44:23 +0000

    modified:   deploy-killer.py 	deleted:    splittest.py

--- deploy-killer.py
+++ deploy-killer.py
@@ -5,7 +5,6 @@
 # simple python script to kill a random K8s pod in every 10 seconds
 # handy to see how deployments and replicasets working
 
-
 import os
 from random import choice
 from time import sleep
@@ -13,18 +12,17 @@ from time import sleep
 to_kill = ""
 
 def find_pod(): 
-  global to_kill
-  podlist = []
-  pods = os.popen("kubectl get pods --no-headers|awk {'print $1'}").read()
-  for i in pods.splitlines(): 
-    podlist.append(i)
-  to_kill = choice(podlist)
+    global to_kill
+    podlist = []
+    pods = os.popen("kubectl get pods --no-headers").read()
+    for i in pods.splitlines(): 
+        podlist.append(i.split(" ")[0])
+    to_kill = choice(podlist)
 
 while True: 
-  find_pod()
-  print("\nKilling pod")
-  os.system(f"kubectl delete pod {to_kill}")
-  print("\nSleeping for 10sec...  hit ctrl+c to stop\n")
-  sleep(10)
-
+    find_pod()
+    print("\nKilling pod")
+    os.system(f"kubectl delete pod {to_kill}")
+    print("\nSleeping for 10sec...  hit ctrl+c to stop\n")
+    sleep(10)
 

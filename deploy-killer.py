51cbe57e47665ff254965ca496a0ee1111063d46
Jacint Juhasz <yyaazz@gmail.com>
2024-04-13 21:13:34 +0000

    remove global and use return instead

--- deploy-killer.py
+++ deploy-killer.py
@@ -9,18 +9,15 @@ import os
 from random import choice
 from time import sleep
 
-to_kill = ""
-
 def find_pod(): 
-    global to_kill
     podlist = []
     pods = os.popen("kubectl get pods --no-headers").read()
     for i in pods.splitlines(): 
         podlist.append(i.split(" ")[0])
-    to_kill = choice(podlist)
+    return choice(podlist)
 
 while True: 
-    find_pod()
+    to_kill = find_pod()
     print("\nKilling pod")
     os.system(f"kubectl delete pod {to_kill}")
     print("\nSleeping for 10sec...  hit ctrl+c to stop\n")

87e71b2de30ceebacbb9f7e10d0945a199325804
Jacint Juhasz <yyaazz@gmail.com>
2024-04-17 20:14:26 +0000

    k8s cluster create scripts

---
+++ delete_cluster.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /home/jacint/.vcps
+
+if [[ $# -eq 0 ]]
+    then echo paramter: cluster1 or cluster2
+else
+    for i in `cat $1`; 
+        do ansible-playbook /home/jacint/ansible/vmware/delete-vm.yaml --extra-vars "vmname=$i password=$PS"; 
+    done
+fi
+
+
--- poweron-cluster.sh
+++ poweron-cluster.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+source /home/jacint/.vcps
+
 if [[ $# -eq 0 ]]
     then echo paramter: cluster1 or cluster2
 else
-    for i in `cat $1`; do ansible-playbook .poweron.yaml --extra-vars "vmname=$i"; done
+    for i in `cat $1`; do ansible-playbook /home/jacint/ansible/vmware/poweron.yaml --extra-vars "vmname=$i password=$PS"; done
 fi
 

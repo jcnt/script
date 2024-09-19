969c445d7c26bd2a80595658ba21611968cb69ed
Jacint Juhasz <yyaazz@gmail.com>
2024-09-19 09:27:23 +0000

    change folder structure between scripts and ansible

--- halt-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $# -eq 0 ]]
-    then echo paramter: cluster1 or cluster2
-else
-    for i in `cat $1`; do ssh $i sudo halt -p; done
-fi
-
--- vminfo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /home/jacint/.vcps
-
-ansible-playbook /home/jacint/ansible/vmware/vminfo.yaml --extra-vars "vmname=$i password=$PS"
-
-

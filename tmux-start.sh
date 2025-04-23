d55e79650afec939e31a88ab82ca4618ff2e5656
Jacint Juhasz <yyaazz@gmail.com>
2025-04-23 22:34:30 +0200

    Lazy commit Wed Apr 23 10:34:30 PM CEST 2025

---
+++ tmux-start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+tmux new-session -d -s 󰣇+ -n 󰞷
+tmux new-window -t 󰣇+ -n 
+tmux new-window -t 󰣇+ -n 󰻞
+tmux new-window -t 󰣇+ -n k8s
+tmux new-window -t 󰣇+ -n k8s
+tmux new-window -t 󰣇+ -n k8s
+tmux new-window -t 󰣇+ -n FA
+tmux new-window -t 󰣇+ -n 
+tmux new-window -t 󰣇+ -n 󰞷
+tmux new-window -t 󰣇+ -n 󰞷

946356bf3eec7db988100cb2ea4338c0298518bd
Jacint Juhasz <yyaazz@gmail.com>
2025-06-06 08:15:22 +0200

    Lazy commit Fri Jun  6 08:15:22 AM CEST 2025

--- automigrate/automigrate-atob.py
+++ automigrate/automigrate-atob.py
@@ -39,49 +39,49 @@ print(f"Migrating jj-automigrate from FAB to FAA...")
 POD = "jj-pod-" + time.strftime("%Y%m%d%H%M%S")
 
 pod = flasharray.PodPost()
-response = fab.post_pods(pod=pod, names=[POD])
+response = faa.post_pods(pod=pod, names=[POD])
 print("Creating Pod...", response.status_code)
 
 container = {"pod": {"name": POD}}
-response = fab.patch_volumes(names=[VOL], volume=container)
+response = faa.patch_volumes(names=[VOL], volume=container)
 print("Moving volume to Pod...", response.status_code)
 
-response = fab.post_pods_arrays(member_names=["PureCZ-FA-A"], group_names=[POD])
+response = faa.post_pods_arrays(member_names=["PureCZ-FA-B"], group_names=[POD])
 print("Adding Pod member...", response.status_code)
 
 podvol = POD + "::" + VOL
-response = faa.get_volumes(names=[podvol])
+response = fab.get_volumes(names=[podvol])
 
 print("waiting for pod sync", end="", flush=True)
 while response.status_code == 400:
     print(".", end="", flush=True)
     time.sleep(1)
-    response = faa.get_volumes(names=[podvol])
-
+    response = fab.get_volumes(names=[podvol])
 
 resp = 400
 while resp != 200:
     print(".", end="", flush=True)
     time.sleep(1)
-    response = faa.post_connections(volume_names=[podvol], host_names=["jjRHEL"])
+    response = fab.post_connections(volume_names=[podvol], host_names=["jjRHEL"])
     resp = response.status_code
 
 print("\nConnecting Vol on second array...", resp)
 
 print("Running rescan on iSCSI paths")
 os.system("ssh rhel sudo iscsiadm -m session --rescan")
+time.sleep(5)
 
-response = fab.delete_connections(volume_names=[podvol], host_names=["jjRHEL"])
+response = faa.delete_connections(volume_names=[podvol], host_names=["jjRHEL"])
 print("Removing host connection on source...", response.status_code)
 
 print("Running rescan on iSCSI paths")
 os.system("ssh rhel sudo iscsiadm -m session --rescan")
 
-response = faa.delete_pods_members(pod_names=[POD], member_names=["PureCZ-FA-B"])
+response = fab.delete_pods_members(pod_names=[POD], member_names=["PureCZ-FA-A"])
 print("Removing source from pod...", response.status_code)
 
 container = {"pod": {"name": ""}}
-response = faa.patch_volumes(names=[podvol], volume=container)
+response = fab.patch_volumes(names=[podvol], volume=container)
 print("Moving volume out from the Pod...", response.status_code)
 
 # response = fab.delete_pods(names=[POD])
